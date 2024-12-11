2a64401bdbf98131fb6e4538bed6266dab100e93
Max <max.kanfjall@outlook.com>
2024-12-11 11:17:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+int number = 0;
+
+for (int i = 0; i < 100; i++)
+{
+    number++;
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+
+    else if(number % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    
+    else if(number % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+
+    else
+    {
+        Console.WriteLine(number);
+    }
+}
